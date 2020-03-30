5eced801bb20a3eb3c82fbd30f2e29be5ce1caee
SDToews <33942967+SDToews@users.noreply.github.com>
2020-03-30 08:00:46 -0700

    daily commit

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const concat = require('gulp-concat')
-gulp.task('default', () => {
-    return gulp
-        .src('./components/*.js')
-        .pipe(concat('index.js'))
-        .pipe(gulp.dest('.'))
-})
