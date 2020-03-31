e2de6e839be439e7703ef71b2c26cf4a0072dbc7
SDToews <33942967+SDToews@users.noreply.github.com>
2020-03-31 08:04:01 -0700

    babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require("path")
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+  externals: {
+    react: "react"
   }
 }
