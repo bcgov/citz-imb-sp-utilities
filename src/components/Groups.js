d43cc1b25f0fa9be5fda88cb7237db0cbc11065a
SDToews <33942967+SDToews@users.noreply.github.com>
2020-04-07 10:34:38 -0700

    GetAssociatedGroups

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -209,3 +209,21 @@ export const CreateGroup = ({ url = '', groupName, groupDescription = '' }) => {
     })
 
 }
+
+export const GetAssociatedGroups = (url = '') => {
+    return new Promise((resolve, reject) => {
+        Promise.all([
+            RestCall({ url: url, endPoint: `/_api/Web/AssociatedOwnerGroup` }),
+            RestCall({ url: url, endPoint: `/_api/Web/AssociatedMemberGroup` }),
+            RestCall({ url: url, endPoint: `/_api/Web/AssociatedVisitorGroup` })
+        ])
+            .then(response => {
+                resolve({
+                    AssociatedOwnerGroup: response[0].d,
+                    AssociatedMemberGroup: response[1].d,
+                    AssociatedVisitorGroup: response[2].d
+                })
+            })
+    })
+
+}
--- src/index.js
+++ src/index.js
@@ -1,32 +1,58 @@
-import { GetContextWebInformation, GetFormDigestValue, GetCurrentUser } from "./components/ContextInfo"
-import { GetGroup, CreateGroup, GetGroupMembers, AddUsersToGroup, RemoveUsersFromGroup } from './components/Groups'
+import {
+	GetContextWebInformation,
+	GetFormDigestValue,
+	GetCurrentUser,
+} from './components/ContextInfo'
+import {
+	GetGroup,
+	CreateGroup,
+	GetGroupMembers,
+	AddUsersToGroup,
+	RemoveUsersFromGroup,
+	GetAssociatedGroups,
+} from './components/Groups'
 import { PeoplePicker } from './components/PeoplePicker'
 import { GetUser, GetUserGroups } from './components/Users'
-import { GetList, GetListItems, AddItemsToList, RemoveItemsFromList, GetListDefaultView, GetListViews, GetListFields } from './components/Lists'
-
+import {
+	GetList,
+	GetListItems,
+	AddItemsToList,
+	RemoveItemsFromList,
+	GetListDefaultView,
+	GetListViews,
+	GetListFields,
+} from './components/Lists'
 
 export {
-    //ContextInfo
-    GetContextWebInformation,
-    GetFormDigestValue,
-    GetCurrentUser,
-    //Groups
-    GetGroup,
-    CreateGroup,
-    GetGroupMembers,
-    AddUsersToGroup,
-    RemoveUsersFromGroup,
-    //Lists
-    GetList,
-    GetListItems,
-    AddItemsToList,
-    RemoveItemsFromList,
-    GetListDefaultView,
-    GetListViews,
-    GetListFields,
-    //PeoplePicker
-    PeoplePicker,
-    //Users
-    GetUser,
-    GetUserGroups
+	//ContextInfo
+	GetContextWebInformation,
+	GetFormDigestValue,
+	GetCurrentUser,
+	//Groups
+	GetGroup,
+	CreateGroup,
+	GetGroupMembers,
+	AddUsersToGroup,
+	RemoveUsersFromGroup,
+	GetAssociatedGroups,
+	// ChangeGroupOwner,
+	//Lists
+	GetList,
+	GetListItems,
+	AddItemsToList,
+	RemoveItemsFromList,
+	GetListDefaultView,
+	GetListViews,
+	GetListFields,
+	// CreateList,
+	//PeoplePicker
+	PeoplePicker,
+	//Permissions
+	// SetPermissionsOnSite,
+	// SetPermissionsOnList,
+	// BreakInheritanceOnList,
+	// RemovePermissionsOnList,
+	//Users
+	GetUser,
+	GetUserGroups,
 }
